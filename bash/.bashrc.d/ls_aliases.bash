2d212b11c5e49f1e25472d19dba9c1061d8340d0
Scott Killen <scott@scottkillen.com>
2018-07-06 02:11:15 -0400

    Change lal to lla

--- bash/.bashrc.d/ls_aliases.bash
+++ bash/.bashrc.d/ls_aliases.bash
@@ -1,6 +1,6 @@
-alias lal='ls -l --almost-all'
 alias ll='ls -l'
+alias lla='ls -l --almost-all'
 alias ls='ls --human-readable -C --classify $__COLOR_OPTIONS'
-alias lsl='lal | less'
+alias lss='lla | less'
 alias dir='dir --human-readable $__COLOR_OPTIONS'
 alias vdir='vdir --human-readable $__COLOR_OPTIONS'
