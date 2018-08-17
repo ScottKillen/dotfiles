950e3087499d40b31ac7a3956b26d45586adae1d
Scott Killen <scott@scottkillen.com>
2018-08-17 00:41:35 -0400

    Reformat environment variable names

--- bash/.bashrc.d/cdable_vars.bash
+++ bash/.bashrc.d/cdable_vars.bash
@@ -1,4 +1,4 @@
-export bash="$HOME/.bashrc.d"
-export bin="$HOME/bin"
-export dotfiles="$HOME/.dotfiles"
-export src="$HOME/src"
+export BASH="$HOME/.bashrc.d"
+export BIN="$HOME/bin"
+export DOTFILES="$HOME/.dotfiles"
+export SRC="$HOME/src"
