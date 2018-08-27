42c6f59def61adfc77d844cf5e57f8bf6845532d
Scott Killen <scott@scottkillen.com>
2018-08-27 18:08:53 -0400

    Add error message

--- bash/.bashrc.d/mcd_function.bash
+++ bash/.bashrc.d/mcd_function.bash
@@ -2,6 +2,11 @@
 # mcd: create directory and move there
 #-------------------------------------------------------------
 mcd () {
+  if [ $# -ne 1 ]; then
+    echo "Usage: mcd <directory-name>"
+    return
+  fi
+
   mkdir --parents $1
   cd $1
 }
